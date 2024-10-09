6699a2e6323807ca3f555890f66e29aaec248207
Felipe <felipe.frug.mauro@gmail.com>
2024-10-09 13:01:33 -0300

    terminado

--- main/main.c
+++ main/main.c
@@ -12,48 +12,46 @@
 
 #include <Fusion.h>
 
+#define UART_ID uart0
+
 const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
 
+typedef struct {
+    int8_t x;
+    int8_t y;
+} mouse_data_t;
+
+QueueHandle_t xQueueMouse;
+
 static void mpu6050_reset() {
-    // Two byte reset. First byte register, second byte data
-    // There are a load more options to set up the device in different ways that could be added here
     uint8_t buf[] = {0x6B, 0x00};
     i2c_write_blocking(i2c_default, MPU_ADDRESS, buf, 2, false);
 }
 
 static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
-    // For this particular device, we send the device the register we want to read
-    // first, then subsequently read from the device. The register is auto incrementing
-    // so we don't need to keep sending the register we want, just the first.
-
     uint8_t buffer[6];
-
-    // Start reading acceleration registers from register 0x3B for 6 bytes
     uint8_t val = 0x3B;
-    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true); // true to keep master control of bus
+
+    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
     i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);
 
     for (int i = 0; i < 3; i++) {
         accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
     }
 
-    // Now gyro data from reg 0x43 for 6 bytes
-    // The register is auto incrementing on each read
     val = 0x43;
     i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);  // False - finished with bus
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);
 
     for (int i = 0; i < 3; i++) {
-        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
     }
 
-    // Now temperature from reg 0x41 for 2 bytes
-    // The register is auto incrementing on each read
     val = 0x41;
     i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 2, false);  // False - finished with bus
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 2, false);
 
     *temp = buffer[0] << 8 | buffer[1];
 }
@@ -66,27 +64,64 @@ void mpu6050_task(void *p) {
     gpio_pull_up(I2C_SCL_GPIO);
 
     mpu6050_reset();
-    int16_t acceleration[3], gyro[3], temp;
+
+    int16_t accel[3], gyro[3], temp;
+    FusionAhrs ahrs;
+    FusionAhrsInitialise(&ahrs);
+
 
     while(1) {
-        mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+        mpu6050_read_raw(accel, gyro, &temp);
 
-        vTaskDelay(pdMS_TO_TICKS(10));
+        FusionVector gyroscope;
+        gyroscope.axis.x = gyro[0] / 131.0f;
+        gyroscope.axis.y = gyro[1] / 131.0f;
+        gyroscope.axis.z = gyro[2] / 131.0f;
+
+        FusionVector accelerometer;
+        accelerometer.axis.x = accel[0] / 16384.0f;
+        accelerometer.axis.y = accel[1] / 16384.0f;
+        accelerometer.axis.z = accel[2] / 16384.0f;
+
+        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, 0.01f);
+
+        const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+
+        uart_putc_raw(UART_ID, 0);
+        uart_putc_raw(UART_ID, (int) euler.angle.pitch & 0xFF);
+        uart_putc_raw(UART_ID, ((int) euler.angle.pitch >> 8) & 0xFF);
+        uart_putc_raw(UART_ID, -1);
+
+        uart_putc_raw(UART_ID, 1);
+        uart_putc_raw(UART_ID, (int) euler.angle.roll & 0xFF);
+        uart_putc_raw(UART_ID, ((int) euler.angle.roll >> 8) & 0xFF);
+        uart_putc_raw(UART_ID, -1);
+
+        // printf("Acc. X = %d, Y = %d, Z = %d - ", accel[0], accel[1], accel[2]);
+        // printf("Gyro. X = %d, Y = %d, Z = %d - ", gyro[0], gyro[1], gyro[2]);
+        // printf("Temp. = %f\n\n\n", (temp / 340.0) + 36.53);
+
+        int mod = abs(accel[1]);
+        
+        if (mod > 17000){
+            uart_putc_raw(UART_ID, 2);
+            uart_putc_raw(UART_ID, 0);
+            uart_putc_raw(UART_ID, ((mod >> 8) & 0xFF));
+            uart_putc_raw(UART_ID, -1);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(10)); 
     }
-
-
 }
 
-int main() {
+void main() {
     stdio_init_all();
 
-    xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
+    xQueueMouse = xQueueCreate(10, sizeof(mouse_data_t));
+
+    xTaskCreate(mpu6050_task, "mpu6050_task", 4096, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
-    while (true)
-        ;
+    while (true);
 }
